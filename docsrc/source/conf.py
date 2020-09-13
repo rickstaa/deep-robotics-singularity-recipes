9a382ee208e88fe89e6e406eba575e6423097ef4
rickstaa <rick.staa@outlook.com>
2020-09-13 12:15:17 +0200

    :bookmark: Bump version: 0.1.0 → 0.1.1

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2019, Rick Staa"
 author = "Rick Staa"
 
 # The full version, including alpha/beta/rc tags
-version = "0.1.0"
+version = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 
